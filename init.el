54a779573a6f2699942b93a2d6c2e5c0469d21c1
Santosh Sivaraj <santosh@fossix.org>
2021-05-30 11:02:40 +0530

    Load file based on their extension
    If org, use babel, else plain load. Remove custom settings from init.el
    
    Signed-off-by: Santosh Sivaraj <santosh@fossix.org>

--- init.el
+++ init.el
@@ -7,16 +7,3 @@
              ;; load up the starter kit
              (org-babel-load-file (expand-file-name "emacs-config.org"
                                                     config-dir))))
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(org-agenda-files nil))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(default ((((class color) (min-colors 89)) (:foreground "#d4d4d4" :background "#000000"))))
- '(variable-pitch ((t (:family "Noto Sans")))))
